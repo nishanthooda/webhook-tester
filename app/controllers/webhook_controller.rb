b61e4f323a2de22122438920c6c7de90d69fc670
Nishant Hooda <nishanthooda@github.com>
2020-06-11 11:18:32 -0400

    webhook tester running

---
+++ app/controllers/webhook_controller.rb
@@ -0,0 +1,55 @@
+require "awesome_print"
+require "rainbow"
+
+class WebhookController < ApplicationController
+    WEBHOOK_HEADERS = ["HTTP_USER_AGENT", "CONTENT_TYPE", "HTTP_X_GITHUB_EVENT", "HTTP_X_GITHUB_DELIVERY", "HTTP_X_HUB_SIGNATURE"]
+
+    before_action :verify_signature!
+
+    def create
+        print_headers_and_body
+        render(status: 200, json: "gotcha")
+    end
+
+    private
+
+    def payload
+      params["webhook"]
+    end
+
+    def print_headers_and_body
+        newline
+        puts Rainbow("~~~~~~~~~~~ webhook arriving for event: #{request.headers["HTTP_X_GITHUB_EVENT"]} ~~~~~~~~~~").cyan
+        newline
+
+
+        WEBHOOK_HEADERS.each do |header|
+            puts "#{header}: #{request.headers[header]}"
+        end
+
+        newline
+        newline
+
+        ap payload.to_unsafe_h
+
+        newline
+        puts Rainbow("-----------------done outputting------------------").cyan
+        newline
+    end
+
+    def newline
+        puts "\n"
+    end
+
+    def verify_signature!
+        secret = ENV["WEBHOOK_SECRET"]
+
+        signature = 'sha1='
+        signature += OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), secret, request.body.read)
+
+        unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+            guid = request.headers["HTTP_X_GITHUB_DELIVERY"]
+            render(status: 400, json: "secret verification failed for #{guid}")
+        end
+    end  
+end
