b8d00a78c31de087536ab1f246854ef420c9a47f
Nishant Hooda <nishanthooda@github.com>
2020-06-15 15:27:29 -0400

    mail to github email

--- app/mailers/merged_mailer.rb
+++ app/mailers/merged_mailer.rb
@@ -5,6 +5,6 @@ class MergedMailer < ApplicationMailer
         @url = params[:url]
         @handle = params[:author_handle]
         puts "repo_ownder_email is #{@repo_owner_email}"
-        mail(to: @repo_owner_email, subject: 'A PR was merged into your repo')
+        mail(to: 'nishanthooda@github.com', subject: 'A PR was merged into your repo')
     end
 end
